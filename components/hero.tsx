bf7a4453216920dfa449d661d706b018f864ec5d
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-03-30 14:03:39 +0800

    Remove next UI and used reacstrap instead

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,10 @@
-import { Card, Container, Row, Text, useTheme } from "@nextui-org/react";
+
 
 export default function Hero (){
-    const { theme } = useTheme()
+
     return (
         <>
-            <Container>
-                <Text style={{ font: theme?.fonts.main }}>
-                    BEllie Joe
-                </Text>
-            </Container>
+        
         </>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,45 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider, createTheme, Theme  } from '@nextui-org/react';
 import Layout from '@/components/layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  const theme = createTheme({
-    type: "dark", // it could be "light" or "dark"
-    theme: {
-      colors: {
-        // brand colors
-        primaryLight: '$green200',
-        primaryLightHover: '$green300',
-        primaryLightActive: '$green400',
-        primaryLightContrast: '$green600',
-        primary: '#4ADE7B',
-        primaryBorder: '$green500',
-        primaryBorderHover: '$green600',
-        primarySolidHover: '$green700',
-        primarySolidContrast: '$white',
-        primaryShadow: '$green500',
-  
-        // gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
-        // link: '#5E1DAD',
-  
-        // // you can also create your own color
-        // myColor: '#ff4ecd'
-  
-        // ...  more colors
-      },
-      space: {},
-      fonts: {
-        mono : "Times New Roman"
-      }
-    }
-  })
+
   
   return (
-    <NextUIProvider theme={theme}>
       <Layout>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </Layout>
-    </NextUIProvider>
   );
 }

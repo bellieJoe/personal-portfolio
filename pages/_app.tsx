f29ece299e7477b773eb7e0379c3ff49ccacd375
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-04-25 11:04:02 +0800

    Modify the importing of google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,10 @@ import type { AppProps } from 'next/app'
 import { NextUIProvider, createTheme, Theme  } from '@nextui-org/react';
 import Layout from '@/components/layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Orbitron } from 'next/font/google';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  
   return (
       <Layout>
         <Component {...pageProps} />

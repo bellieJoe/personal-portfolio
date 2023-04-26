1735a839c0839691256f7303017cfe5d90a6bf8d
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-04-26 21:57:16 +0800

    Refine the hero and interest section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,11 +3,10 @@ import styles from "./Hero.module.scss";
 import {motion} from "framer-motion";
 
 export default function Hero(){
-    console.log(styles)
     return (
         <>
             <div className={styles.Hero} id="hero">
-                <motion.div whileInView={{ y: 0, opacity: 1}} initial={{y:'2rem', opacity: 0}} transition={{duration: .3, ease: "easeOut"}}>
+                <motion.div whileInView={{ y: 0, opacity: 1}} initial={{y:'4rem', opacity: 0}} transition={{duration: .3, ease: "easeOut"}}>
                     <div className={styles.headText}>
                         <h1>Building the future, one line of code at a time</h1>
                     </div>

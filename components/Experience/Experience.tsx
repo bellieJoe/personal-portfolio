839b697ab37a582681696a464c0fa2f01e8e1fc8
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-04-27 22:05:22 +0800

    Change the layout of Experience section

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -1,11 +1,13 @@
 import SectionHeader from "../SectionHeader/SectionHeader"
 import styles from "./Experience.module.scss"
+import {motion, Variants} from "framer-motion";
 
 function PathCard(props : any){
     return (
         <div className={styles.PathCard}>
-            <h4>{props.title}</h4>
-            <p>{props.description}</p>
+            <h3 className={styles.role}>{props.role}</h3>
+            <p className={styles.companyName}>{props.companyName}</p>
+            <p className={styles.desc}>{props.description}</p>
         </div>
     )
 }
@@ -15,10 +17,11 @@ function Path(props : any){
         <div className={styles.Path}>
             <div className="">
                 <h3 className={styles.scope}>{props.scope}</h3>
-                <p className={styles.companyName}>{props.companyName}</p>
-                <p className={styles.role}>{props.role}</p>
             </div>
-            <PathCard title={props.title} description={props.description} />
+            <PathCard title={props.title} 
+                    companyName={props.companyName}
+                    role={props.role} 
+                    description={props.description} />
         </div>
     )
 }
@@ -29,7 +32,8 @@ export default function Experience(){
             scope: '2015-2017',
             companyName: 'Polaris Mold Fabrication Corporation',
             role: 'EDM Technician',
-            description: 'As an EDM Technician, I operated Electrical Discharge Machines to cut and modify metal shapes based on client blueprints. This job required precision and attention to detail to avoid defects. I learned to use precision measurement tools and program EDM machines with specialized software. By applying the knowledge I learned from high school subjects such as Math, Mechanical, and Technical Drafting, I gained a solid foundation for understanding principles and calculations. This job also taught me the importance of communication and teamwork. Overall, my experience as an EDM Technician provided me with a wealth of knowledge and skills, including precision, technical knowledge, communication, and collaboration, which are essential qualities for success in any field.',
+            description: 'Working as an EDM Technician, I operated Electrical Discharge Machines to cut and modify metal shapes according to client blueprints. This role demanded precision and attention to detail, as well as the ability to program EDM machines with specialized software. It taught me the importance of teamwork, communication, and technical knowledge. Overall, this experience provided me with a solid foundation for understanding principles and calculations in any field.',
+            // description: 'As an EDM Technician, I operated Electrical Discharge Machines to cut and modify metal shapes based on client blueprints. This job required precision and attention to detail to avoid defects. I learned to use precision measurement tools and program EDM machines with specialized software. By applying the knowledge I learned from high school subjects such as Math, Mechanical, and Technical Drafting, I gained a solid foundation for understanding principles and calculations. This job also taught me the importance of communication and teamwork. Overall, my experience as an EDM Technician provided me with a wealth of knowledge and skills, including precision, technical knowledge, communication, and collaboration, which are essential qualities for success in any field.',
             title: '-dscrptn'
         },
         {
@@ -42,15 +46,31 @@ export default function Experience(){
         {
             scope: '2022-Present',
             companyName: 'Marinduque State College - ICTSC',
-            role: 'Computer Programmer',
+            role: 'Full Stack Developer',
             description: "After graduating from my alma mater, I was fortunate to land my first job as a Software Developer in the same institution. In this role, I was responsible for designing and developing Information Systems that were beneficial to the institution. It was also an excellent opportunity for me to apply the knowledge I had recently acquired in PHP and Laravel development. Additionally, it was my first time collaborating with other software developers working on similar systems, which helped me develop my teamwork and communication skills.",
             title: '-dscrptn'
         },
-    ]
+    ];
+    const headerVariant : Variants = {
+        show: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                type: "spring",
+            }
+        },
+        hidden: {
+            opacity: 0,
+            x: "-4rem"
+        }
+    }
 
     return (
         <div className={styles.Experience} id="experiences">
-            <SectionHeader text="My Career Paths..." />
+            <motion.div  variants={headerVariant} initial="hidden" whileInView="show">
+                <SectionHeader text={<span>My <span style={{ color: 'rgb(0, 255, 157)' }}>Career</span> Path & Experience</span>} />
+            </motion.div>
+            
             {
                 experiences.map(exp => (
                     <Path key={exp.scope} scope={exp.scope}

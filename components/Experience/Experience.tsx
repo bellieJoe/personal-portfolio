f46f2fb6c7585a8b156ec522d90f08dafd85bbec
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-05-06 22:58:46 +0800

    Detached the exeperience and interest data to the corresponding component

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -4,6 +4,7 @@ import styles from "./Experience.module.scss"
 import {motion, useScroll, Variants} from "framer-motion";
 import path from "path";
 import zIndex from "@mui/material/styles/zIndex";
+import experienceData from "./ExperienceData";
 
 function PathCard(props : any){
     return (
@@ -74,30 +75,7 @@ function Path(props : any){
 }
 
 export default function Experience(){
-    const experiences = [
-        {
-            scope: '2015-2017',
-            companyName: 'Polaris Mold Fabrication Corporation',
-            role: 'EDM Technician',
-            description: 'Working as an EDM Technician, I operated Electrical Discharge Machines to cut and modify metal shapes according to client blueprints. This role demanded precision and attention to detail, as well as the ability to program EDM machines with specialized software. It taught me the importance of teamwork, communication, and technical knowledge. Overall, this experience provided me with a solid foundation for understanding principles and calculations in any field.',
-            // description: 'As an EDM Technician, I operated Electrical Discharge Machines to cut and modify metal shapes based on client blueprints. This job required precision and attention to detail to avoid defects. I learned to use precision measurement tools and program EDM machines with specialized software. By applying the knowledge I learned from high school subjects such as Math, Mechanical, and Technical Drafting, I gained a solid foundation for understanding principles and calculations. This job also taught me the importance of communication and teamwork. Overall, my experience as an EDM Technician provided me with a wealth of knowledge and skills, including precision, technical knowledge, communication, and collaboration, which are essential qualities for success in any field.',
-            title: '-dscrptn'
-        },
-        {
-            scope: '2017-2022',
-            companyName: 'Marinduque State College - Boac Campus',
-            role: 'IT Student',
-            description: 'College was a turning point in my career journey, as I pursued my passion for IT by taking a 4-year bachelor\'s course in MSC. Throughout the program, I gained extensive knowledge and skills in software development and engineering, which was the career path I had always envisioned for myself. Thanks to my hard work and dedication, I successfully completed the course with excellent performance and earned valuable credentials that helped me kickstart my IT career.',
-            title: '-dscrptn'
-        },
-        {
-            scope: '2022-Present',
-            companyName: 'Marinduque State College - ICTSC',
-            role: 'Full Stack Developer',
-            description: "After graduating from my alma mater, I was fortunate to land my first job as a Software Developer in the same institution. In this role, I was responsible for designing and developing Information Systems that were beneficial to the institution. It was also an excellent opportunity for me to apply the knowledge I had recently acquired in PHP and Laravel development. Additionally, it was my first time collaborating with other software developers working on similar systems, which helped me develop my teamwork and communication skills.",
-            title: '-dscrptn'
-        },
-    ];
+    const experiences = experienceData;
     const headerVariant : Variants = {
         show: {
             opacity: 1,

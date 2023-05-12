bd6d6ff8b91a6bcbafc4c20fb9bf8bedb90f8b6c
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-05-12 09:18:04 +0800

    Added google verification meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,7 @@ class MyDocument extends Document {
           {CssBaseline.flush()}
           <meta name="description" content="Bellie Joe Jandusay is a Software Developer based on the Philippines. Equip with different skills necessary for software development such as React, Laravel, MySQL, Ionic etc." />
           <meta name="keywords" content="Bellie, Bellie Joe, Bellie Joe Jandusay, Laravel Developer, React Developer, Software Developer, Web Developer, Full Stack Developer" />
+          <meta name="google-site-verification" content="jZJCryi0BISLzVdsPo61ud4yVBIPHQeiKOdF4iC9maY" />
           <title>Bellie Joe Jandusay - Portfolio</title>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"

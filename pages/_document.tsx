7645258e97e653f2fee2d6b90de6d38199993eac
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2023-05-12 09:04:54 +0800

    Added meta tags for SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,9 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {CssBaseline.flush()}
+          <meta name="description" content="Bellie Joe Jandusay is a Software Developer based on the Philippines. Equip with different skills necessary for software development such as React, Laravel, MySQL, Ionic etc." />
+          <meta name="keywords" content="Bellie, Bellie Joe, Bellie Joe Jandusay, Laravel Developer, React Developer, Software Developer, Web Developer, Full Stack Developer" />
+          <title>Bellie Joe Jandusay - Portfolio</title>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
